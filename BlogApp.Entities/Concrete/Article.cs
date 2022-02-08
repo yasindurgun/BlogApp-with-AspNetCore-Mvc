c9735ac7f9044568b7c067ad48b19209444c20aa
yasindurgun <yasin.durgun52@gmail.com>
2022-02-08 12:30:00 +0300

    Add project files.

---
+++ BlogApp.Entities/Concrete/Article.cs
@@ -0,0 +1,29 @@
+﻿using BlogApp.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Entities.Concrete
+{
+    public class Article : EntityBase, IEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Thumnail { get; set; }
+        public DateTime Date { get; set; }
+        public int ViewsCount { get; set; }
+        public int CommentsCount  { get; set; }
+        public string SeoAuthor { get; set; }
+        public string SeoDescription { get; set; }
+        public string SeoTags { get; set; }
+        public int CategoryId { get; set; }
+        //navigation property
+        public Category Category { get; set; }
+        public int UserId { get; set; }
+        //navigation property
+        public User User { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
---
+++ BlogApp.Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Entities.Concrete
+{
+    public class Category : EntityBase, IEntity
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; } // posts
+    }
+}
