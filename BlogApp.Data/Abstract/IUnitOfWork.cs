79ba9bbe45b76e9ef0501a1ff1104ff0bdf79b26
yasindurgun <yasin.durgun52@gmail.com>
2022-02-25 02:10:38 +0300

    Add UnitOfWork implementations.

---
+++ BlogApp.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Data.Abstract
+{
+    //tüm repositoryler tek bir yerden yönetilir
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IArticleRepository Articles { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+        Task<int> SaveAsync();
+    }
+}
