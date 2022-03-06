e51b36cc18f8032f5c821b811a79366802790070
yasindurgun <yasin.durgun52@gmail.com>
2022-03-06 16:44:57 +0300

    Create result structure and ICategoryService.

---
+++ BlogApp.Services/Abstract/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using BlogApp.Entities.Concrete;
+using BlogApp.Entities.Dtos;
+using BlogApp.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Services.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<IDataResult<Category>> Get(int categoryId);
+        Task<IDataResult<List<Category>>> GetAll();
+        Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int categoryId);
+        Task<IResult> HardDelete(int categoryId);
+    }
+}
---
+++ BlogApp.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,34 @@
+﻿using BlogApp.Shared.Utilities.Results.Abstract;
+using BlogApp.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Shared.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+        public Result(ResultStatus resultStatus, string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public Result(ResultStatus resultStatus, string message, Exception ex)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Exception = ex;
+        }
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }
+
+        public Exception Exception { get; }
+    }
+}
